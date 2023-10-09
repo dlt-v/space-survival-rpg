7495ce618c3d8c4d8b3f102b094aacf036f00e49
dlt-v <tmm.michalski@gmail.com>
2023-10-09 23:21:32 +0200

    add intro

--- src/main/java/com/spacesurvival/GameController.java
+++ src/main/java/com/spacesurvival/GameController.java
@@ -8,9 +8,10 @@ public class GameController {
     int fuel = 0;
     int oxygen = 100;
 
+    private Scanner scanner = new Scanner(System.in);
+
     public GameController() throws InterruptedException {
         System.out.print("Enter your name: ");
-        Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine(); // Scanner goes to next line.
         player = new Player(name);
         System.out.println("Player " + player.getName() + " created.");
@@ -20,6 +21,24 @@ public class GameController {
     public void startGame() {
         System.out.print("Game started. Good luck!");
         Util.cls();
+        printIntro();
+    }
 
+    public void printIntro() {
+        StringBuilder intro = new StringBuilder();
+        intro.append("You are an astronaut sent to an alien planet in order to establish a new base.\n");
+        intro.append("During your landing attempt you encountered a critical system error.\n");
+        intro.append("An error which caused you to crash.\n\n");
+        intro.append("You are now stranded on this alien world.\n");
+        intro.append("Your main directive is to survive and, if possible, escape the planet.\n\n");
+        System.out.print(intro);
+        System.out.print("Acknowledge: [ENTER] ");
+        scanner.nextLine();
+    }
+
+    public void closeGame() throws InterruptedException {
+        System.out.println("Game will now close.");
+        scanner.close();
+        Thread.sleep(500);
     }
 }
--- src/main/java/com/spacesurvival/Main.java
+++ src/main/java/com/spacesurvival/Main.java
@@ -6,5 +6,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         GameController gameController = new GameController();
         gameController.startGame();
+
+        gameController.closeGame();
     }
 }
