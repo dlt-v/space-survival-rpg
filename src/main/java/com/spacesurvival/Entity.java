7fbc06b1039669978154ba2105590f3a1c854bbf
dlt-v <tmm.michalski@gmail.com>
2023-10-09 22:29:08 +0200

    create Entity abstract class and Player class

---
+++ src/main/java/com/spacesurvival/Entity.java
@@ -0,0 +1,25 @@
+package com.spacesurvival;
+
+public abstract class Entity {
+    private String name = null;
+    protected int hitPoints = 1;
+
+    public Entity (String name) {
+        this.name = name;
+    }
+
+    void regainHealth(int value) {
+        hitPoints = Math.min(hitPoints + value, 100);
+    }
+    void takeDamage(int damage) {
+        hitPoints = Math.max(hitPoints - damage, 0);
+    }
+
+    String getName() {
+        return name;
+    }
+
+    int getHitPoints() {
+        return hitPoints;
+    }
+}
