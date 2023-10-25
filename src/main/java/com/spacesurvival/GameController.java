a2c1e7d6b307350755982df83d1f8fed0dd7596b
dlt-v <tmm.michalski@gmail.com>
2023-10-25 19:54:06 +0200

    add primitive main game loop

--- src/main/java/com/spacesurvival/GameController.java
+++ src/main/java/com/spacesurvival/GameController.java
@@ -9,6 +9,8 @@ public class GameController {
     int fuel = 0;
     int oxygen = 100;
 
+    boolean gameRunning = true;
+
     private Scanner scanner = new Scanner(System.in);
 
     public GameController() throws InterruptedException {
@@ -34,6 +36,57 @@ public class GameController {
         intro.append("Your main directive is to survive and, if possible, escape the planet.\n\n");
         System.out.print(intro);
         cls(true);
+
+        startMainLoop();
+    }
+
+    private void startMainLoop() {
+
+        while(gameRunning) {
+            StringBuilder mainLoopOptions = new StringBuilder();
+            mainLoopOptions.append("What would you like to do:\n");
+            mainLoopOptions.append("[1] - Check your equipment.\n");
+            mainLoopOptions.append("[2] - Go outside and look for resources.\n");
+            mainLoopOptions.append("[3] - Check your blueprints.\n");
+            mainLoopOptions.append("[4] - Rest and regenerate health.\n");
+            mainLoopOptions.append("[0] - Exit game.\n");
+            mainLoopOptions.append("Enter option number: ");
+            System.out.print(mainLoopOptions);
+            int playerChoice;
+            try {
+                playerChoice = scanner.nextInt();
+            } catch (Exception e) {
+                System.out.println("Something went wrong: " + e + "\nTry again.");
+                continue;
+            } finally {
+                scanner.nextLine();
+            }
+
+            switch(playerChoice) {
+                case 1 -> {
+                    System.out.println("Checking equipment...");
+                }
+                case 2 -> {
+                    System.out.println("Looking for resources...");
+                }
+                case 3 -> {
+                    System.out.println("Checking blueprints...");
+                }
+                case 4 -> {
+                    System.out.println("Resting...");
+                }
+                case 0 -> {
+                    System.out.println("Stopping game...");
+                    gameRunning = false;
+                }
+                default -> {
+                    System.out.println("Invalid choice. Try again.");
+                }
+
+            }
+            cls(true);
+        }
+
 
     }
 
